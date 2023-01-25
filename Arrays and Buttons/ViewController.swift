e2fe87c88d16a9f04ba40d937e7bd34fed69d803
Glenda Mullan <glenda.mullan@gmail.com>
2023-01-25 16:50:47 +1100

    completed

--- Arrays and Buttons/ViewController.swift
+++ Arrays and Buttons/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var glenTonFamily = ["Toni", "Glenda", "Chris", "Holly", "Stewit"]
+    
+    var messageNumber = 0
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print(glenTonFamily)
+        messageLabel.text = "Press the Button"
     }
-
-
+    
+    
+    @IBAction func button(_ sender: UIButton) {
+        //messageLabel.text = "Press the button"
+        messageLabel.textColor = UIColor.red
+        messageLabel.textAlignment = NSTextAlignment.center
+        
+        messageLabel.text = glenTonFamily[messageNumber]
+        messageNumber += 1
+        if messageNumber == glenTonFamily.count{
+            messageNumber = 0
+        }
+    }
 }
-
